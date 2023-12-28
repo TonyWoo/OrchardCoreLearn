21d5ec4bb6b6fbcf7bb5a181cf2f503e8769b7f1
Tony Wu <tonywoo815@gmail.com>
2023-12-28 20:47:57 +0800

    initial

---
+++ OrchardCore.Themes/NewAge/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "NewAge",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "NewAge"
+)]
